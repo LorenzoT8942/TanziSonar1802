6df0d49a764a69ca58f94513f3af0c37c2d599f4
LorenzoT8942 <56634954+LorenzoT8942@users.noreply.github.com>
2021-02-18 16:11:56 +0100

    first commit

---
+++ src/logic/SimpleCalculator.java
@@ -0,0 +1,29 @@
+package logic;
+
+public class SimpleCalculator {
+	
+	public double add(double a, double b) {
+		return a + b;
+	}
+	
+	public double sottraction(double a, double b) {
+		return a - b;
+	}
+	
+	public double mult(double a, double b) {
+		return a*b;
+	}
+	
+	public double division(double a, double b) {
+		return a/b;
+	}
+	
+	public double log(double a) {
+		return Math.log10(a);
+	}
+	
+	public double sqrt(double a) {
+		return Math.sqrt(a);
+	}	
+	
+}
